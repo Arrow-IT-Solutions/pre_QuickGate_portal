4517497ae09ff64d132e58c4a5b3b88a784e4b99
Nour <noursaleem@arrowitjo.com>
2025-06-19 16:13:29 +0300

    edit settings page

--- src/app/modules/QR/qr.module.ts
+++ src/app/modules/QR/qr.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { QRCodeDialogComponent } from './qrcode-dialog/qrcode-dialog.component';
-import { QRCodeComponent } from 'angularx-qrcode';
+// import { QRCodeComponent } from 'angularx-qrcode';
+import { QRCodeModule } from 'angularx-qrcode';
 import { MatButtonModule } from '@angular/material/button';
 import { SharedModule } from 'src/app/shared/shared.module';
 
@@ -10,14 +11,14 @@ import { SharedModule } from 'src/app/shared/shared.module';
 @NgModule({
   declarations: [QRCodeDialogComponent],
   imports: [
-    CommonModule,
     MatDialogModule,
     MatButtonModule,
-    QRCodeComponent,
+    QRCodeModule,
+    SharedModule,
     SharedModule,
   ],
   exports: [
-    QRCodeDialogComponent
+    QRCodeDialogComponent    // ← if other modules need to open it
   ]
 })
 

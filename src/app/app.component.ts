c936eaad41be24fd4f12fa7ce000c7d9e827d39f
Zer093 <105790577+Zer093@users.noreply.github.com>
2025-08-25 16:42:17 +0300

    Anonymous User Issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { AdhostDirective } from './Core/directive/adhost.directive';
 import { DynamicDilogComponent } from './layout/component/dynamic-dilog/dynamic-dilog.component';
 import { TranslateService } from '@ngx-translate/core';
@@ -6,7 +6,8 @@ import { UserService } from './Core/services/user.service';
 
 import { LayoutService } from './layout/service/layout.service';
 import { Direction } from '@angular/cdk/bidi';
-
+import { LocalService } from './shared/service/local.service';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,15 +19,32 @@ export class AppComponent {
   constructor(
     public translate: TranslateService,
     public userService: UserService,
-    public layoutService: LayoutService
-  ) {}
+    public layoutService: LayoutService,
+    public localService: LocalService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.userService.currentUser = {
+      userUUID: "response.uuid",
+      userName: "response.userName",
+      userType: "response.userType",
+      token: "response.token",
+      loggedInUser: "response.userLoggedIn"
+    };
+    this.localService.saveData('currentUser', JSON.stringify(this.userService.currentUser));
+    this.localService.GetStorge();
+
+    this.translate.use(this.layoutService.config.lang);
+
+    this.document.documentElement.lang = this.layoutService.config.lang;
+    console.log('app ts ngoninit');
+  }
 
   ngOnInit(): void {
-    console.log('app ts ngoninit');
+
 
   }
 
- 
+
 
   getDirection(): Direction | 'auto' {
     return this.layoutService.config.lang === 'ar' ? 'rtl' : 'ltr';
